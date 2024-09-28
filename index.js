d4c6c1c5e06c495eea1fa761159b55217ca3882f
Ian Kabaka <iankabaka1@gmail.com>
2024-09-28 22:39:43 +0300

    find method in arrays

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+// function superbowlWin(record) {
+//   for (let convertObj of record)
+//     if (convertObj.result === "W") {
+//       return convertObj.year;
+//     }
+// }
+// superbowlWin(record);
+
+function superbowlWin(objectRecord) {
+  const isCorrect = objectRecord.find((obj) => obj.result === "W");
+  return isCorrect ? isCorrect.year : undefined;
+}
